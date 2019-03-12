f498f14d0a86dac4e3bc882b2c142d557b5af079
lbolesta <lbolesta@luc.edu>
2019-03-11 22:34:29 -0500

    Add project files.

---
+++ AlgorithmPractice/GenerateBalancedParen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPractice
+{
+    public class GenerateBalancedParen
+    {
+        private IList strlist = new List<string>();
+        public IList GenerateParenthesis(int n)
+        {
+            GenerateParenthesisHelper(n, n, string.Empty);
+            return strlist;
+        }
+
+        private void GenerateParenthesisHelper(int left, int right, string result)
+        {
+            if (left == 0 && right == 0)
+            {
+                strlist.Add(result);
+                return;
+            }
+
+            if (left > 0)
+            {
+                GenerateParenthesisHelper(left - 1, right, result + '(');
+            }
+
+            if (right > left)
+            {
+                GenerateParenthesisHelper(left, right - 1, result + ')');
+            }
+        }
+    }
+}
---
+++ AlgorithmPractice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AlgorithmPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paren = new GenerateBalancedParen();
+            var result = paren.GenerateParenthesis(2);
+            foreach(var r in result)
+            {
+                Console.WriteLine(r);
+            }
+
+            var str = "aabbccdddfff";
+            var freq = GetFrequencyOfCharsInString(str);
+            foreach(var f in freq)
+            {
+                Console.WriteLine(f.Key + " " + f.Value);
+            }
+            
+
+        }
+
+        private new static Dictionary<char, int> GetFrequencyOfCharsInString(string str)
+        {
+            var result = new Dictionary<char, int>();
+            var dict = new Dictionary<char, int>();
+
+            var charArray = str.ToCharArray();
+            foreach (var c in charArray)
+            {
+                if (dict.ContainsKey(c)) { dict[c] += 1; }
+                else { dict.Add(c, 1); }
+            }
+
+            var max = dict.Values.Max();
+
+            var res = dict.Where(x => x.Value == dict.Values.Max())
+                .Select(x => new { key = x.Key, value = x.Value });
+            var ret = res.ToDictionary(x => x.key);
+            foreach (var r in ret)
+                result.Add(r.Key, r.Value.value);
+            return result;
+
+
+        }
+    }
+}
